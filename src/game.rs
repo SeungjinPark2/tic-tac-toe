c7ccf5b03513ad976e891c2ccaf9cdf4d89e9b1d
aad3365 <aad336545@gmail.com>
2023-07-25 23:58:43 +0900

    game.rs 1차 구현 완료

--- src/game.rs
+++ src/game.rs
@@ -1,11 +1,12 @@
 use core::fmt;
-
+#[derive(PartialEq)]
 pub enum CellState {
     O,
     X,
     NONE,
 }
 
+// struct 으로 만들긴 했으나 굳이 그럴필요가 없는 단일 필드만 갖는 녀석이 되어 버렸음.
 pub struct tic_tac_toe {
     pub board: Vec<Vec<CellState>>, 
 }
@@ -47,11 +48,37 @@ impl tic_tac_toe {
 
     // 일시적으로 CellState 으로 Winner 를 구분하도록 한다.
     // 추후에 Player struct 를 만들어서 구분하자.
-    fn check_board(&self) -> CellState{
+    pub fn check_board(&self) -> &CellState {
         // 일단 승리 조건을 거의 하드코딩에 가깝게 구현하자.
         // 목적은 게임을 만드는것.
-
-        CellState::NONE
+        // 난 개 멍청한 것 같다 이렇게 하지 않으면 모르게따ㅏㅏㅏㅏ
+        if self.board[0][0] == self.board[0][1]
+            && self.board[0][1] == self.board[0][2] {
+            &self.board[0][0]
+        } else if self.board[1][0] == self.board[1][1]
+            && self.board[1][1] == self.board[1][2] {
+            &self.board[1][0]
+        } else if self.board[2][0] == self.board[2][1]
+            && self.board[2][1] == self.board[2][2] {
+            &self.board[2][0]
+        } else if self.board[0][0] == self.board[1][0]
+            && self.board[1][0] == self.board[2][0] {
+            &self.board[0][0]
+        } else if self.board[0][1] == self.board[1][1]
+            && self.board[1][1] == self.board[2][1] {
+            &self.board[0][1]
+        } else if self.board[0][2] == self.board[1][2]
+            && self.board[1][2] == self.board[2][2] {
+            &self.board[0][2]
+        } else if self.board[0][0] == self.board[1][1]
+            && self.board[1][1] == self.board[2][2] {
+            &self.board[0][0]
+        } else if self.board[0][2] == self.board[1][1]
+            && self.board[1][1] == self.board[2][0] {
+            &self.board[0][2]
+        } else {
+            &CellState::NONE
+        }
     }
 }
 
--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,37 @@
 mod game;
 
 use game::{Point, CellState};
+use std::io;
 
 fn main() {
     let mut myTTT = game::create_tic_tac_toe();
+    let mut toggle = CellState::O;
 
-    println!("{}", myTTT.get_board_summary());
+    loop {
+        println!("{}", myTTT.get_board_summary());
+        let mut line = String::from("");
+        io::stdin().read_line(&mut line).unwrap();
+        
+        let point_as_vec = line.split(',').collect::<Vec<&str>>();
+        let point = Point { x: point_as_vec[0].parse().unwrap(), y: point_as_vec[1].parse().unwrap() };
 
-    myTTT.make_move(Point {x: 0, y: 0}, CellState::O).unwrap();
+        // make_move 가 CellState 를 받아서 어쩔수 없이 우회하는데 Copy trait 을 구현하던, 참조를 받게 만들던 하는게 맞음.
+        match toggle {
+            CellState::O => myTTT.make_move(point, CellState::O).unwrap(),
+            CellState::X => myTTT.make_move(point, CellState::X).unwrap(),
+            _ => (),
+        }
 
-    println!("{}", myTTT.get_board_summary());
+        match myTTT.check_board() {
+            &CellState::O => {
+                println!("winner is O");
+                break;
+            },
+            &CellState::X => {
+                println!("winner is X");
+                break;
+            },
+            _ => ()
+        }
+    }
 }
